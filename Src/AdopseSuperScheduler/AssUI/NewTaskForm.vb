ebe5e5f682b8f12fddb7bab3dad1a14047afc6d5
Stavros Bizelis <bizelis@it.teithe.gr>
2012-12-19 17:12:26 +0200

    1)added In NEWTASKFORM in the save button -> passed parameter of close_after

--- Src/AdopseSuperScheduler/AssUI/NewTaskForm.vb
+++ Src/AdopseSuperScheduler/AssUI/NewTaskForm.vb
@@ -411,19 +411,19 @@ Public Class NewTaskForm
 
             If OnceCheckButton.Checked Then
                 m_master_control.AddTask(chooseFileTextBox.Text, m_date, m_end_date, MoreOptionsForm.ActiveRadioButton.Checked, m_type, _
-                MoreOptionsForm.DescriptionTextBox.Text, 0, 0, 0, 0, m_not_run)
+                MoreOptionsForm.DescriptionTextBox.Text, Integer.Parse(MoreOptionsForm.MinutesUpDown.Value), 0, 0, 0, m_not_run)
 
             ElseIf DailyCheckButton.Checked Then
                 m_master_control.AddTask(chooseFileTextBox.Text, m_date, m_end_date, MoreOptionsForm.ActiveRadioButton.Checked, m_type, _
-                MoreOptionsForm.DescriptionTextBox.Text, KryptonNumericUpDown1.Value, KryptonNumericUpDown1.Value, 0, 0, m_not_run)
+                MoreOptionsForm.DescriptionTextBox.Text, Integer.Parse(MoreOptionsForm.MinutesUpDown.Value), KryptonNumericUpDown1.Value, 0, 0, m_not_run)
 
             ElseIf WeeklyCheckButton.Checked Then
                 m_master_control.AddTask(chooseFileTextBox.Text, m_date, m_end_date, MoreOptionsForm.ActiveRadioButton.Checked, m_type, _
-                MoreOptionsForm.DescriptionTextBox.Text, KryptonNumericUpDown1.Value, 0, KryptonNumericUpDown1.Value, 0, m_not_run)
+                MoreOptionsForm.DescriptionTextBox.Text, Integer.Parse(MoreOptionsForm.MinutesUpDown.Value), KryptonNumericUpDown1.Value, 0, 0, m_not_run)
 
             Else
                 m_master_control.AddTask(chooseFileTextBox.Text, m_date, m_end_date, MoreOptionsForm.ActiveRadioButton.Checked, m_type, _
-                MoreOptionsForm.DescriptionTextBox.Text, KryptonNumericUpDown1.Value, 0, 0, KryptonNumericUpDown1.Value, m_not_run)
+                MoreOptionsForm.DescriptionTextBox.Text, Integer.Parse(MoreOptionsForm.MinutesUpDown.Value), 0, 0, KryptonNumericUpDown1.Value, m_not_run)
             End If
 
             'Opens up a dialog show that the task was Successfully added
