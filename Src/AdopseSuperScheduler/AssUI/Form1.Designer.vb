3d04a1a7e3d08285656808b3a541761c7a080d84
kdiam <kdiam.adopse@gmail.com>
2013-01-07 21:11:16 +0200

    try to fix previous commit's problem

---
+++ Src/AdopseSuperScheduler/AssUI/Form1.Designer.vb
@@ -0,0 +1,212 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AboutForm
+    Inherits ComponentFactory.Krypton.Toolkit.KryptonForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.KryptonPanel = New ComponentFactory.Krypton.Toolkit.KryptonPanel()
+        Me.KryptonLinkLabel1 = New ComponentFactory.Krypton.Toolkit.KryptonLinkLabel()
+        Me.KryptonLabel5 = New ComponentFactory.Krypton.Toolkit.KryptonLabel()
+        Me.KryptonLabel4 = New ComponentFactory.Krypton.Toolkit.KryptonLabel()
+        Me.KryptonLabel3 = New ComponentFactory.Krypton.Toolkit.KryptonLabel()
+        Me.KryptonLabel2 = New ComponentFactory.Krypton.Toolkit.KryptonLabel()
+        Me.KryptonLabel1 = New ComponentFactory.Krypton.Toolkit.KryptonLabel()
+        Me.KryptonButton1 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.KryptonManager = New ComponentFactory.Krypton.Toolkit.KryptonManager(Me.components)
+        Me.KryptonLabel6 = New ComponentFactory.Krypton.Toolkit.KryptonLabel()
+        Me.KryptonLabel7 = New ComponentFactory.Krypton.Toolkit.KryptonLabel()
+        Me.KryptonLabel8 = New ComponentFactory.Krypton.Toolkit.KryptonLabel()
+        Me.KryptonLabel9 = New ComponentFactory.Krypton.Toolkit.KryptonLabel()
+        Me.KryptonLabel10 = New ComponentFactory.Krypton.Toolkit.KryptonLabel()
+        CType(Me.KryptonPanel, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.KryptonPanel.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'KryptonPanel
+        '
+        Me.KryptonPanel.Controls.Add(Me.KryptonLabel10)
+        Me.KryptonPanel.Controls.Add(Me.KryptonLabel9)
+        Me.KryptonPanel.Controls.Add(Me.KryptonLabel8)
+        Me.KryptonPanel.Controls.Add(Me.KryptonLabel7)
+        Me.KryptonPanel.Controls.Add(Me.KryptonLabel6)
+        Me.KryptonPanel.Controls.Add(Me.KryptonLinkLabel1)
+        Me.KryptonPanel.Controls.Add(Me.KryptonLabel5)
+        Me.KryptonPanel.Controls.Add(Me.KryptonLabel4)
+        Me.KryptonPanel.Controls.Add(Me.KryptonLabel3)
+        Me.KryptonPanel.Controls.Add(Me.KryptonLabel2)
+        Me.KryptonPanel.Controls.Add(Me.KryptonLabel1)
+        Me.KryptonPanel.Controls.Add(Me.KryptonButton1)
+        Me.KryptonPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.KryptonPanel.Location = New System.Drawing.Point(0, 0)
+        Me.KryptonPanel.Name = "KryptonPanel"
+        Me.KryptonPanel.Size = New System.Drawing.Size(728, 401)
+        Me.KryptonPanel.TabIndex = 0
+        '
+        'KryptonLinkLabel1
+        '
+        Me.KryptonLinkLabel1.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.ItalicPanel
+        Me.KryptonLinkLabel1.Location = New System.Drawing.Point(464, 123)
+        Me.KryptonLinkLabel1.Name = "KryptonLinkLabel1"
+        Me.KryptonLinkLabel1.Size = New System.Drawing.Size(252, 22)
+        Me.KryptonLinkLabel1.TabIndex = 6
+        Me.KryptonLinkLabel1.Values.Text = "https://code.google.com/p/adopse-scheduler/"
+        '
+        'KryptonLabel5
+        '
+        Me.KryptonLabel5.Location = New System.Drawing.Point(499, 105)
+        Me.KryptonLabel5.Name = "KryptonLabel5"
+        Me.KryptonLabel5.Size = New System.Drawing.Size(175, 22)
+        Me.KryptonLabel5.TabIndex = 5
+        Me.KryptonLabel5.Values.Text = "Google Code Repository URL :"
+        '
+        'KryptonLabel4
+        '
+        Me.KryptonLabel4.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.ItalicControl
+        Me.KryptonLabel4.Location = New System.Drawing.Point(12, 183)
+        Me.KryptonLabel4.Name = "KryptonLabel4"
+        Me.KryptonLabel4.Size = New System.Drawing.Size(362, 22)
+        Me.KryptonLabel4.TabIndex = 4
+        Me.KryptonLabel4.Values.Text = "Πρόγραμμα χρονοπρογραμματισμού εκτέλεσης προγραμμάτων"
+        '
+        'KryptonLabel3
+        '
+        Me.KryptonLabel3.Location = New System.Drawing.Point(432, 47)
+        Me.KryptonLabel3.Name = "KryptonLabel3"
+        Me.KryptonLabel3.Size = New System.Drawing.Size(175, 22)
+        Me.KryptonLabel3.TabIndex = 3
+        Me.KryptonLabel3.Values.Text = "Χειμερινό εξάμηνο 2012-2013"
+        '
+        'KryptonLabel2
+        '
+        Me.KryptonLabel2.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.SuperTip
+        Me.KryptonLabel2.Location = New System.Drawing.Point(341, 25)
+        Me.KryptonLabel2.Name = "KryptonLabel2"
+        Me.KryptonLabel2.Size = New System.Drawing.Size(384, 26)
+        Me.KryptonLabel2.TabIndex = 2
+        Me.KryptonLabel2.Values.Text = "Αλεξάνδρειο Τεχνολογικό Εκπαιδευτικό Ίδρυμα Θεσσαλονίκης"
+        '
+        'KryptonLabel1
+        '
+        Me.KryptonLabel1.Location = New System.Drawing.Point(3, 3)
+        Me.KryptonLabel1.Name = "KryptonLabel1"
+        Me.KryptonLabel1.Size = New System.Drawing.Size(436, 174)
+        Me.KryptonLabel1.TabIndex = 1
+        Me.KryptonLabel1.Values.Image = Global.AssUI.My.Resources.Resources.AssLogo
+        Me.KryptonLabel1.Values.Text = ""
+        '
+        'KryptonButton1
+        '
+        Me.KryptonButton1.Location = New System.Drawing.Point(313, 364)
+        Me.KryptonButton1.Name = "KryptonButton1"
+        Me.KryptonButton1.Size = New System.Drawing.Size(90, 25)
+        Me.KryptonButton1.TabIndex = 0
+        Me.KryptonButton1.Values.Text = "Close"
+        '
+        'KryptonManager
+        '
+        Me.KryptonManager.GlobalPaletteMode = ComponentFactory.Krypton.Toolkit.PaletteModeManager.ProfessionalSystem
+        '
+        'KryptonLabel6
+        '
+        Me.KryptonLabel6.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.ItalicControl
+        Me.KryptonLabel6.Location = New System.Drawing.Point(68, 200)
+        Me.KryptonLabel6.Name = "KryptonLabel6"
+        Me.KryptonLabel6.Size = New System.Drawing.Size(240, 22)
+        Me.KryptonLabel6.TabIndex = 7
+        Me.KryptonLabel6.Values.Text = " και διαχείρισης του ιστορικού εκτέλεσης."
+        '
+        'KryptonLabel7
+        '
+        Me.KryptonLabel7.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.BoldControl
+        Me.KryptonLabel7.Location = New System.Drawing.Point(332, 229)
+        Me.KryptonLabel7.Name = "KryptonLabel7"
+        Me.KryptonLabel7.Size = New System.Drawing.Size(54, 22)
+        Me.KryptonLabel7.TabIndex = 8
+        Me.KryptonLabel7.Values.Text = "Ομάδα:"
+        '
+        'KryptonLabel8
+        '
+        Me.KryptonLabel8.Location = New System.Drawing.Point(185, 257)
+        Me.KryptonLabel8.Name = "KryptonLabel8"
+        Me.KryptonLabel8.Size = New System.Drawing.Size(387, 22)
+        Me.KryptonLabel8.TabIndex = 9
+        Me.KryptonLabel8.Values.Text = "Μπιζέλης Σταύρος (bizelis@it.teithe.gr /polemiosolon@hotmail.com)"
+        '
+        'KryptonLabel9
+        '
+        Me.KryptonLabel9.Location = New System.Drawing.Point(170, 276)
+        Me.KryptonLabel9.Name = "KryptonLabel9"
+        Me.KryptonLabel9.Size = New System.Drawing.Size(427, 22)
+        Me.KryptonLabel9.TabIndex = 10
+        Me.KryptonLabel9.Values.Text = "Διαμαντίδης Κωνσταντίνος (kdiam@it.teithe.gr /kdiam.adopse@gmail.com)"
+        '
+        'KryptonLabel10
+        '
+        Me.KryptonLabel10.Location = New System.Drawing.Point(179, 295)
+        Me.KryptonLabel10.Name = "KryptonLabel10"
+        Me.KryptonLabel10.Size = New System.Drawing.Size(393, 22)
+        Me.KryptonLabel10.TabIndex = 11
+        Me.KryptonLabel10.Values.Text = "Δεληγίαννης Νικόλαος (ndelig@it.teithe.gr /ndelig.teithe@gmail.com)"
+        '
+        'AboutForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(728, 401)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.KryptonPanel)
+        Me.MaximizeBox = False
+        Me.Name = "AboutForm"
+        Me.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.ProfessionalSystem
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "About Adopse Super Scheduler"
+        CType(Me.KryptonPanel, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.KryptonPanel.ResumeLayout(False)
+        Me.KryptonPanel.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents KryptonPanel As ComponentFactory.Krypton.Toolkit.KryptonPanel
+    Friend WithEvents KryptonManager As ComponentFactory.Krypton.Toolkit.KryptonManager
+
+    Public Sub New()
+
+        ' This call is required by the Windows Form Designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+    End Sub
+    Friend WithEvents KryptonButton1 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents KryptonLabel1 As ComponentFactory.Krypton.Toolkit.KryptonLabel
+    Friend WithEvents KryptonLabel2 As ComponentFactory.Krypton.Toolkit.KryptonLabel
+    Friend WithEvents KryptonLinkLabel1 As ComponentFactory.Krypton.Toolkit.KryptonLinkLabel
+    Friend WithEvents KryptonLabel5 As ComponentFactory.Krypton.Toolkit.KryptonLabel
+    Friend WithEvents KryptonLabel4 As ComponentFactory.Krypton.Toolkit.KryptonLabel
+    Friend WithEvents KryptonLabel3 As ComponentFactory.Krypton.Toolkit.KryptonLabel
+    Friend WithEvents KryptonLabel10 As ComponentFactory.Krypton.Toolkit.KryptonLabel
+    Friend WithEvents KryptonLabel9 As ComponentFactory.Krypton.Toolkit.KryptonLabel
+    Friend WithEvents KryptonLabel8 As ComponentFactory.Krypton.Toolkit.KryptonLabel
+    Friend WithEvents KryptonLabel7 As ComponentFactory.Krypton.Toolkit.KryptonLabel
+    Friend WithEvents KryptonLabel6 As ComponentFactory.Krypton.Toolkit.KryptonLabel
+End Class
